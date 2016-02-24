7be6e08a0973f556beb295e4019b6a753904f3e6
lennyparisi1 <lennyparisi4@gmail.com>
2016-02-23 21:18:05 -0500

    login/register

--- socialSite.js
+++ socialSite.js
@@ -1,23 +1,70 @@
+var email,pass;
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    
+    Router.configure({
+        layoutTemplate:"main"    
+    });
+    
+    Router.route("/login",{
+        name: 'login',
+        template: 'login'            
+    });
+    Router.route("/register",{
+        name: 'register',
+        template: 'register'
+    });    
+    
+    Router.route('/', function() {
+        this.render("login")
+    });
+    
+    Template.login.events({
+        "click .logOut":function(event){
+            event.preventDefault();
+            Meteor.logout();
+            Router.go("login");
+        }
+    });
+    
+    Template.register.events({
+        "submit form":function(event){
+            event.preventDefault();
+            email = $('[name=registerEmail]').val();
+            pass = $('[name=registerPassword]').val();
+            event.target.registerEmail.value = "";
+            event.target.registerPassword.value = "";
+            Accounts.createUser({
+                email:email,
+                password:pass
+            });
+            Router.go("login");
+        }
+    });
+    
+    Template.login.events({
+        'submit #logInForm': function(event) {
+            event.preventDefault();
+            email = $('[name=loginEmail]').val();
+            pass = $('[name=loginPassword]').val();
+            event.target.loginEmail.value = "";
+            event.target.loginPassword.value = "";
+            Meteor.loginWithPassword(email, pass);
+            
+        }
+    });
+    
+    Accounts.onLoginFailure(function(){
+        console.log("failed to log in");  
+    });
+    
+    Accounts.onLogin(function(){
+        console.log("logged in");
+    });    
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        
+    });
 }
